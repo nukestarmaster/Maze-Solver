9470500db99197f725d50cbb34e191747cb1be79
nukestarmaster <nukestarmaster@gmail.com>
2024-03-20 14:39:57 -0700

    Refactored window.py into graphics.py and implemented point and line classes

--- data/graphics.py
+++ data/graphics.py
@@ -22,4 +22,26 @@ class window:
     def close(self):
         self.running = False
     
-    
+    def draw_line(self, line, fill_colour):
+        line.draw(self.canvas, fill_colour)
+
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+     
+    def draw(self, canvas, fill_colour):
+        canvas.create_line(
+            self.a.x,
+            self.a.y,
+            self.b.x,
+            self.b.y,
+            fill = fill_colour,
+            width = 2
+        )
+        canvas.pack()
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from data.window import window
+from data.graphics import window, line, point
+
+width = 800
+height = 600
+
+line1 = line(point(0,0), point(width, height))
+
 
 def main():
-    win = window(800, 600)
+    win = window(width, height)
+    win.draw_line(line1, "red")
     win.wait_for_close()
 
 main()
